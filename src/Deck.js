9e243c976a14831e83cf7059e1ee008007edf809
Nicole Valentini <ncvalentini@gmail.com>
2022-02-01 21:17:26 -0700

    Add test skeleton and base implementation code

---
+++ src/Deck.js
@@ -0,0 +1,12 @@
+
+const data = require('./data');
+
+class Deck {
+  constructor(data) {
+    this.cards = data;
+    this.number = 30
+  }
+
+};
+
+module.exports = Deck;
--- test/Deck-test.js
+++ test/Deck-test.js
@@ -1,2 +1,36 @@
 const chai = require('chai');
 const expect = chai.expect;
+const data = require('../src/data');
+
+const Deck = require('../src/Deck');
+const Card = require('../src/Card');
+
+describe('Deck', function() {
+
+  it('should be a function', () => {
+    const deck = new Deck();
+    expect(Deck).to.be.a('function');
+  });
+
+  it('should be an instance of Deck',() => {
+    const deck = new Deck();
+    expect(deck).to.be.an.instanceof(Deck);
+  })
+
+  it ('should contain an array of cards', () => {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    expect(deck.number).to.equal(30)
+  })
+})
+
+//   it.skip('should know how many Cards are in the Deck', () => {
+//     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+//     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+//     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+//     const deck = new Deck([card1, card2, card3]);
+//     expect(deck.length.length).to.equal(3)
+//   })
+ // })
