7472d5af61603ad7866f660fe534bb2c75b7cc2b
Nicole Valentini <ncvalentini@gmail.com>
2022-02-02 23:20:51 -0700

    Add game constructor and related tests

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,12 @@ const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
 
+const Round = require('../src/Round')
+
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound = new Round(deck)
+  }
 
   printMessage(deck, round) {
     console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -1,2 +1,27 @@
 const chai = require('chai');
 const expect = chai.expect;
+const Game = require('../src/Game')
+const Round = require('../src/Round')
+const Deck = require('../src/Deck')
+const Card = require('../src/Card')
+const Turn = require('../src/Turn')
+
+describe('Round', function() {
+
+beforeEach(() => {
+card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+deck = new Deck([card1, card2, card3])
+round = new Round(deck);
+game = new Game();
+});
+
+  it('should be a function', function() {
+    expect(Game).to.be.a('function');
+  });
+
+  it('should be an instance of Round', function() {
+    expect(game).to.be.an.instanceof(Game);
+  });
+});
