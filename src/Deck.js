7687070559f5cb0d553868ccca21373ed847c96e
Nicole Valentini <ncvalentini@gmail.com>
2022-02-03 18:44:31 -0700

    Remove uncessary require

--- src/Deck.js
+++ src/Deck.js
@@ -1,15 +1,11 @@
-
-const data = require('./data');
-const prototypeQuestions = data.prototypeData;
-
 class Deck {
   constructor(cards) {
     this.cards = cards;
-  }
+  };
 
   countCards(){
     return this.cards.length
-  }
+  };
 };
 
 module.exports = Deck;
